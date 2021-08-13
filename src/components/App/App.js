b282049b1676c9eb963ced1393cff261438ccc61
pierrefrancoispaoletti <pierre.paoletti1@gmail.com>
2021-08-14 01:01:53 +0200

    adding confidentiality page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { Route, Switch } from "react-router";
-import { Divider, Message, Transition } from "semantic-ui-react";
+import { Container, Divider, Message, Transition } from "semantic-ui-react";
 import Categories from "../../pages/Categories";
 import Home from "../../pages/Home";
 import { $SERVER } from "../../_const/_const";
@@ -192,6 +192,45 @@ const App = () => {
                 setOpenEditProductModal={setOpenEditProductModal}
                 setSelectedProduct={setSelectedProduct}
               />
+            </Route>
+            <Route path="/confidentialite-de-lapp">
+              <Container style={{color: "white", fontSize:"1.5em", textAlign: "center", lineHeight: "1.5em"}}>
+
+              <h2>Engagement de confidentialité</h2>
+
+
+La protection de vos données est importante pour le Baravin1755 Le présent engagement de confidentialité s’applique à la façon dont nous collectons, utilisons, divulguons, transférons et conservons vos données. Veuillez prendre le temps de vous familiariser avec nos pratiques en matière de protection des données à caractère personnel et contactez-nous si vous avez des questions.
+
+<h2>Collecte et utilisation des données à caractère personnel</h2>
+Les données personnelles (ou « informations personnelles ») sont des données qui peuvent être utilisées pour identifier de manière unique ou contacter une seule personne.
+
+<h2>
+Collecte et utilisation des données à caractère non-
+Divulgation à des tiers
+</h2>
+
+Aucune donnée n’est divulguée a des tiers.
+
+<h2>Protection des données à caractère personnels</h2>
+Le Baravin1755 prend des précautions – y compris des mesures administratives, techniques et physiques – pour protéger vos données personnelles contre la perte, le vol et la mauvaise utilisation, ainsi que l’accès, la divulgation, l’altération et la destruction non autorisés.
+
+<h2>
+
+Intégrité et conservation des données à caractère personnel
+</h2>
+Vos données ne sont pas conservées, dés que votre réservation est traitée (que vous recevez le mail de confirmation), vos données sont effacées de nos serveurs de façon programmatique.
+
+<h2>
+
+Questions sur la confidentialité
+</h2>
+Si vous avez des questions ou des inquiétudes concernant le traitement de vos données ou l’Engagement de confidentialité d’Apple, contactez-nous.
+
+
+
+
+
+              </Container>
             </Route>
           </Switch>
           <Divider />
