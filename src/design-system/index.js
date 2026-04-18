fb7d70a794d2516859d8221b51703a72e5926e95
PAOLETTI Pierre-François <pf.paoletti@zeendoc.com>
2026-04-18 15:15:40 +0200

    feat(ds): barrel d'exports (tokens, fonts, composants, icônes)

---
+++ src/design-system/index.js
@@ -0,0 +1,6 @@
+import "./tokens.css";
+import "./fonts";
+
+export { default as Button } from "./components/Button";
+export { default as TabBar } from "./components/TabBar";
+export { ICON_MAP, PICKER_ICONS, PICKER_COLORS, getIcon } from "./iconMap";
